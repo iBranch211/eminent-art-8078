86dbca8997e29ea163178d3ea69f3c0ef95fb931
iBranch211 <leo.esaki211@gmail.com>
2023-03-29 20:20:34 +0530

    AdminPage responsiveness

--- src/Pages/Payments.jsx
+++ src/Pages/Payments.jsx
@@ -1,20 +1,9 @@
 import React from 'react'
-import { useState } from 'react';
-import AddressForm from './Payment/AddressForm';
-import PaymentForm from './Payment/PaymentForm'
-import Checkout from './Payment/Checkout';
-import Review from './Payment/Review';
-import Poster from "./Payment/right.png"
-import { Box } from '@mui/system';
-const Payments = () => {
- 
-  
 
+const Payments = () => {
   return (
-    <div className="container">
-     <Checkout/> 
-    </div>
-  );
+    <div>Payments</div>
+  )
 }
 
 export default Payments
