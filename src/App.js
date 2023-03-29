13db98137597592c0f0f978e2c1d2665a6e17b7b
iBranch211 <leo.esaki211@gmail.com>
2023-03-29 20:23:55 +0530

    day 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import logo from "./logo.svg";
-import "./App.css";
-
-// import Homepage from "./Pages/Homepage";
-import Navigationbar from "./Components/HomePage/Navigationbar";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Navigationbar />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
 export default App;
+
+{/*  */}
