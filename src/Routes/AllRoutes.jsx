989305c7238ee058cd71294ff7abbd7fb1c7c45f
iBranch211 <leo.esaki211@gmail.com>
2023-03-30 21:00:13 +0530

    fw21_0901_day_3

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -4,9 +4,7 @@ import { Route, Routes } from 'react-router-dom'
 import Homepage from '../Pages/Homepage'
 import LoginPage from '../Pages/LoginPage'
 import NotFoundPage from '../Pages/NotFoundPage'
-import ProductPage from '../Pages/ProductPage'
 import Registration from '../Pages/Registration'
-import { SingleProductPage } from '../Pages/SingleProductPage'
 import { AdminPage } from './../Pages/Admin/AdminPage';
 import Payments from './../Pages/Payments';
 
@@ -19,8 +17,6 @@ const AllRoutes = () => {
 <Route path='*' element={ <NotFoundPage/>} />
 <Route path='/adminpage' element={ <AdminPage/>} />
 <Route path='/payments' element={ <Payments/>} />
-<Route path="/products/:path/:category?/:sub_category?/:brand?"  element={ <ProductPage/> }  />
-<Route  path="/products/:path/:category?/:sub_category?/:id/single?"  element={<SingleProductPage/> }  />
 
 </Routes>
 
