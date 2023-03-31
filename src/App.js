6dd8fc8488b4b486665a6702d6490f175c45dadb
iBranch211 <leo.esaki211@gmail.com>
2023-03-31 17:36:15 +0530

    Day-4

--- src/App.js
+++ src/App.js
@@ -3,11 +3,29 @@ import logo from "./logo.svg";
 import "./App.css";
 
 
+import Homepage from './Pages/Homepage';
+import Navigationbar from './Components/HomePage/Navigationbar';
+
+import LoginPage from './Pages/LoginPage';
+import { SingleProductPage } from './Pages/SingleProductPage';
+import { Home } from './Pages/Home';
+
+import AllRoutes from './Routes/AllRoutes';
+import Banner from './Components/HomePage/Banner';
+import { AdminButton } from "./Pages/Admin/AdminButton";
+
 
 function App() {
   return (
     <div className="App">
 
+      <Home />
+
+
+
+
+      <AllRoutes /> 
+   
     </div>
   );
 }
--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -4,14 +4,11 @@ import { Route, Routes } from 'react-router-dom'
 import Homepage from '../Pages/Homepage'
 import LoginPage from '../Pages/LoginPage'
 import NotFoundPage from '../Pages/NotFoundPage'
-import ProductPage from '../Pages/ProductPage'
 import Registration from '../Pages/Registration'
-import { SingleProductPage } from '../Pages/SingleProductPage'
 import { AdminPage } from './../Pages/Admin/AdminPage';
 import Payments from './../Pages/Payments';
 
 const AllRoutes = () => {
-
   return (
     <Routes>
 <Route path='/' element={<Homepage/> } />
@@ -20,8 +17,6 @@ const AllRoutes = () => {
 <Route path='*' element={ <NotFoundPage/>} />
 <Route path='/adminpage' element={ <AdminPage/>} />
 <Route path='/payments' element={ <Payments/>} />
-<Route path="/products/:path/:category?/:sub_category?/:brand?"  element={ <ProductPage/> }  />
-<Route  path="/products/:path/:category?/:sub_category?/:id/single?"  element={<SingleProductPage/> }  />
 
 </Routes>
 
