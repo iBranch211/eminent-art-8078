059363b716426a7103690ad69b9fe4d13c00f50b
iBranch211 <leo.esaki211@gmail.com>
2023-04-02 13:12:04 +0530

    bugs fixing

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import Navigationbar from "./Components/HomePage/Navigationbar";
 import { Home } from "./Pages/Home";
 import { Wishlist } from "./Pages/Wishlist";
 import AllRoutes from "./Routes/AllRoutes";
+import ProductPage from './Pages/productpage/ProductPage';
+import CartPage from "./Pages/cart/CartPage";
+import Navmain from "./Components/HomePage/Navmain";
 
 function App() {
   return (
     <div className="App">
-      {/* <Navigationbar/> */}
+    <Navmain/>
+    {" "}
    <AllRoutes/>
     </div>
   );
