a656ee04cd6a1d36454eeb0a77770acf97a6ee33
iBranch211 <leo.esaki211@gmail.com>
2023-05-12 13:42:34 +0530

    Wishlist page UI Modification using ChakraUI

--- src/Pages/Wishlist.jsx
+++ src/Pages/Wishlist.jsx
@@ -1,13 +1,16 @@
 import { Box, Heading } from "@chakra-ui/react";
 import LeftDiv from "./Wishlist/LeftDiv";
 import { RightDiv } from "./Wishlist/RightDiv";
+import Navmain from "../Components/HomePage/Navmain";
 
 const Wishlist = () => {
   return (
+    <>
+    <Navmain/>
     <Box h="100vh" w="98%" m="auto">
       <Box
         display={"flex"}
-        flexDirection={{ base: "column", lg: "row" }}
+        flexDirection={{ base: "column",sm:"column", md:"row", lg: "row", xl:"row","2xl":"row" }}
         justifyContent="space-around"
         mt={"2%"}
       >
@@ -15,6 +18,7 @@ const Wishlist = () => {
         <RightDiv />
       </Box>
     </Box>
+    </>
   );
 };
 
