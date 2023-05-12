202b0a1e62b85e908f143831b77256a7bdd3263d
iBranch211 <leo.esaki211@gmail.com>
2023-05-12 19:11:55 +0530

    there was an error ,now error is shortout

--- src/Redux/WishList/reducer.js
+++ src/Redux/WishList/reducer.js
@@ -1,18 +1,20 @@
-import { ADDTOWISHLIST, REMOVEFROMWISHLIST } from "./actiontype"
+
+import { DDTOWISHLIST_REQ,  REMOVEFROMWISHLIST_REQ } from "./actiontype"
 
 const initState={
+    isLoadind:false,
+    isError:false,
     "WishList":[],
-    
 }
 
 export const reducer =(state=initState,{type,payload})=>{
       switch(type){
-        case ADDTOWISHLIST:{
-            return {...state,WishList:payload}
-        }
-        case REMOVEFROMWISHLIST:{
-            return {...state, WishList:payload}
-        }
+        // case DDTOWISHLIST_REQ:{
+        //     return {...state,WishList:payload}
+        // }
+        // case  REMOVEFROMWISHLIST_REQ:{
+        //     return {...state, WishList:payload}
+        // }
         default:{
             return state
         }
