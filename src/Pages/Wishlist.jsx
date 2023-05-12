6f41a78c7f103d2e34721b72f23c963ee54135b4
iBranch211 <leo.esaki211@gmail.com>
2023-05-12 14:13:03 +0530

    cartpage header

--- src/Pages/Wishlist.jsx
+++ src/Pages/Wishlist.jsx
@@ -1,16 +1,13 @@
 import { Box, Heading } from "@chakra-ui/react";
 import LeftDiv from "./Wishlist/LeftDiv";
 import { RightDiv } from "./Wishlist/RightDiv";
-import Navmain from "../Components/HomePage/Navmain";
 
 const Wishlist = () => {
   return (
-    <>
-    <Navmain/>
     <Box h="100vh" w="98%" m="auto">
       <Box
         display={"flex"}
-        flexDirection={{ base: "column",sm:"column", md:"row", lg: "row", xl:"row","2xl":"row" }}
+        flexDirection={{ base: "column", lg: "row" }}
         justifyContent="space-around"
         mt={"2%"}
       >
@@ -18,7 +15,6 @@ const Wishlist = () => {
         <RightDiv />
       </Box>
     </Box>
-    </>
   );
 };
 
--- src/Pages/Wishlist/LeftDiv.jsx
+++ src/Pages/Wishlist/LeftDiv.jsx
@@ -1,10 +1,10 @@
-import { Box, Button, HStack, Stack, Text } from "@chakra-ui/react";
+import { border, Box, Button, HStack, Stack, Text } from "@chakra-ui/react";
 import { RxAvatar } from "react-icons/rx";
 import { TbNotes } from "react-icons/tb";
 import { HiOutlineCurrencyRupee } from "react-icons/hi";
 import { AiOutlineStar } from "react-icons/ai";
 import { AiOutlineHeart } from "react-icons/ai";
-import { BsHouseGear } from "react-icons/bs";
+import { BsCursor, BsHouseGear } from "react-icons/bs";
 import { BsHeadset } from "react-icons/bs";
 import { useEffect, useState } from "react";
 import axios from "axios";
@@ -15,21 +15,12 @@ export default function LeftDiv() {
       setItemCount(res.data.length);
     });
   });
-
-// base: "0px",
-//   sm: "30em", // 480px
-//   md: "48em", // 768px
-//   lg: "62em", // 992px
-//   xl: "80em", // 1280px
-//   "2xl": "96em", // 1536px
-
   return (
     <Box
-      width={{ base: "70%",sm:"50%", md:"40%", lg: "40%", xl:"28%","2xl":"28%" }}  //{ base: "column",sm:"column", md:"row", lg: "row", xl:"row","2xl":"row" }
+      width={{ base: "98%", lg: "28%" }}
       boxShadow={"rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;"}
       fontSize={{ base: "sm", md: "md", lg: "md", xl: "md", "2xl": "md" }}
-      margin={"auto"}
-     >
+    >
       <Stack p="3%">
         <HStack mb="20px" w={"100%"} justifyContent="space-around">
           <Text color="teal">My Account</Text>
@@ -37,8 +28,8 @@ export default function LeftDiv() {
             ml="20%"
             bg="orange.400"
             color="white"
-            size={["2xs", "sm", "md", "md","sm"]}
-            fontSize={{ base: "2xs",sm:"sm", md:"sm", lg: "sm", xl:"sm","2xl":"sm" }}
+            size={["2xs", "sm", "md", "md"]}
+            fontSize={["xs", "sm", "md", "md", "md"]}
           >
             Join VIP Club
           </Button>
@@ -46,32 +37,30 @@ export default function LeftDiv() {
 
         <HStack pl={"10%"} w={"55%"} justifyContent={"space-between"}>
           <TbNotes size="25px" justifyContent="space-around" />
-          <Text pl="15%" 
-          fontSize={{ base: "2xs",sm:"sm", md:"sm", lg: "sm", xl:"sm","2xl":"sm" }}>My Orders</Text>
+          <Text pl="15%">My Orders</Text>
         </HStack>
 
-        <HStack justifyContent="space-around" w={"100%"} paddingLeft={"3%"}>
+        <HStack justifyContent="space-around" w={"100%"} marginLeft={"30%"}>
           <HiOutlineCurrencyRupee size={"25px"} />
-          <Text ml="20px"
-          fontSize={{ base: "2xs",sm:"sm", md:"sm", lg: "sm", xl:"sm","2xl":"sm" }}>My Clues Bucks</Text>
+          <Text ml="20px">My Clues Bucks</Text>
           <Button
             color={"white"}
             bg={"teal.400"}
             size={["2xs", "sm", "md", "md"]}
-            fontSize={{ base: "2xs",sm:"sm", md:"sm", lg: "sm", xl:"sm","2xl":"sm" }}
+            fontSize={["xs", "sm", "md", "md", "md"]}
           >
             0
           </Button>
         </HStack>
 
-        <HStack justifyContent="space-around"  w={"100%"} paddingLeft={"3%"}>
+        <HStack justifyContent="space-around">
           <HiOutlineCurrencyRupee size={"25px"} />
-          <Text ml="20px"  fontSize={{ base: "2xs",sm:"sm", md:"sm", lg: "sm", xl:"sm","2xl":"sm" }}>My Clues Bucks+</Text>
+          <Text ml="20px">My Clues Bucks+</Text>
           <Button
             color={"white"}
             bg={"teal.400"}
             size={["2xs", "sm", "md", "md"]}
-            fontSize={{ base: "2xs",sm:"sm", md:"sm", lg: "sm", xl:"sm","2xl":"sm" }}
+            fontSize={["xs", "sm", "md", "md", "md"]}
           >
             0
           </Button>
@@ -79,23 +68,17 @@ export default function LeftDiv() {
 
         <HStack pl={"10%"} w={"55%"} justifyContent={"space-between"}>
           <RxAvatar size="25px" />
-          <Text pl="15%"
-          fontSize={{ base: "2xs",sm:"sm", md:"sm", lg: "sm", xl:"sm","2xl":"sm" }}
-          >My Profile</Text>
+          <Text pl="15%">My Profile</Text>
         </HStack>
 
         <HStack pl={"10%"} w={"65%"} justifyContent={"space-between"}>
           <AiOutlineStar size="25px" />
-          <Text pl="15%"
-          fontSize={{ base: "2xs",sm:"sm", md:"sm", lg: "sm", xl:"sm","2xl":"sm" }}
-          >Rate Your Purchase</Text>
+          <Text pl="15%">Rate Your Purchase</Text>
         </HStack>
 
         <HStack justifyContent="space-around">
           <AiOutlineHeart size="25px" />
-          <Text
-          fontSize={{ base: "2xs",sm:"sm", md:"sm", lg: "sm", xl:"sm","2xl":"sm" }}
-          >MY Wishlist</Text>
+          <Text>MY Wishlist</Text>
           <Button
             color={"white"}
             bg={"teal.400"}
@@ -108,16 +91,12 @@ export default function LeftDiv() {
 
         <HStack pl={"10%"} w={"65%"} justifyContent={"space-between"}>
           <BsHouseGear size="25px" />
-          <Text pl="15%"
-          fontSize={{ base: "2xs",sm:"sm", md:"sm", lg: "sm", xl:"sm","2xl":"sm" }}
-          >My Favorite Stories</Text>
+          <Text pl="15%">My Favorite Stories</Text>
         </HStack>
 
         <HStack pl={"10%"} w={"65%"} justifyContent={"space-between"}>
           <BsHeadset size="25px" />
-          <Text pl="15%"
-          fontSize={{ base: "2xs",sm:"sm", md:"sm", lg: "sm", xl:"sm","2xl":"sm" }}
-          >Help & Support</Text>
+          <Text pl="15%">Help & Support</Text>
         </HStack>
       </Stack>
     </Box>
