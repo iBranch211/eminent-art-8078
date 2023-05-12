4f47c8bbf753aaeaf0d1ba859867b79908d80cf0
iBranch211 <leo.esaki211@gmail.com>
2023-05-12 12:03:03 +0530

    filtering on brands added

--- src/Pages/Wishlist.jsx
+++ src/Pages/Wishlist.jsx
@@ -1,12 +1,9 @@
 import { Box, Heading } from "@chakra-ui/react";
 import LeftDiv from "./Wishlist/LeftDiv";
 import { RightDiv } from "./Wishlist/RightDiv";
-import Navmain from "../Components/HomePage/Navmain";
 
 const Wishlist = () => {
   return (
-    <>
-    <Navmain/>
     <Box h="100vh" w="98%" m="auto">
       <Box
         display={"flex"}
@@ -18,7 +15,6 @@ const Wishlist = () => {
         <RightDiv />
       </Box>
     </Box>
-    </>
   );
 };
 
